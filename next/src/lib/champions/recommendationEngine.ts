9a8d1fc63e022b635de066aa2df0c048ee584f28
Mark Janzer <mark.janzer@gmail.com>
2025-01-12 11:31:15 -0800

    Port champion recommender to javascript

--- next/src/lib/champions/recommendationEngine.ts
+++ next/src/lib/champions/recommendationEngine.ts
@@ -1,14 +1,93 @@
-import { ChampionPerformance, ChampionDataMap, ChampSelect } from '../types/champion';
+import { ChampionPerformance, ChampionInteraction, ChampionDataMap, ChampSelect } from '../types/champions';
 
 export async function recommendChampions(
   championStats: ChampionDataMap,
-  champSelect: ChampSelect,
-  allChampIds: number[]
+  champSelect: ChampSelect
 ): Promise<ChampionPerformance[]> {
-  // Port the RecommendChampions logic here
-  // Note: You'll need to modify the logic to work without the SQL queries
-  console.log("recommendChampions", championStats, champSelect, allChampIds);
+  const allChampIds = Object.keys(championStats).map(Number)
+  const unavailableChampIds = [...champSelect.bans, ...champSelect.allies, ...champSelect.enemies]
 
+  const results: ChampionPerformance[] = []
+
+  for (const champId of allChampIds) {
+    if (unavailableChampIds.includes(champId)) {
+      continue
+    }
+
+    const championPerformance: ChampionPerformance = {
+      championId: champId,
+      winProbability: -1,
+      synergies: [],
+      matchups: [],
+    }
+
+    for (const allyId of champSelect.allies) {
+      const synergy = championStats[champId].synergies[allyId]
+      if (!synergy) {
+        console.error(`Synergy not found for champion ${champId} and ally ${allyId}`)
+        continue
+      }
+
+      let winProbability: number = 0.50
+      if (synergy.games > 0) {
+        // Smoothing winrate
+        winProbability = (synergy.wins + 5) / (synergy.games + 10)
+      }
+
+      const interaction: ChampionInteraction = {
+        championId: allyId,
+        winProbability: winProbability,
+        wins: synergy.wins,
+        games: synergy.games,
+      }
+
+      championPerformance.synergies.push(interaction)
+    }
+
+    for (const enemyId of champSelect.enemies) {
+      const matchup = championStats[champId].matchups[enemyId]
+      if (!matchup) {
+        console.error(`Matchup not found for champion ${champId} and enemy ${enemyId}`)
+        continue
+      }
+
+      let winProbability: number = 0.50
+      if (matchup.games > 0) {
+        winProbability = (matchup.wins + 5) / (matchup.games + 10)
+      }
+
+      const interaction: ChampionInteraction = {
+        championId: enemyId,
+        winProbability: winProbability,
+        wins: matchup.wins,
+        games: matchup.games,
+      }
+
+      championPerformance.matchups.push(interaction)
+    }
+
+    let winProbability: number = 0.0
+    const dataPoints = championPerformance.synergies.length + championPerformance.matchups.length
+    if (dataPoints > 0) {
+      for (const synergy of championPerformance.synergies) {
+        winProbability += synergy.winProbability
+      }
+
+      for (const matchup of championPerformance.matchups) {
+        winProbability += matchup.winProbability
+      }
+
+      winProbability /= dataPoints
+    } else {
+      winProbability = 0.50
+    }
+
+    championPerformance.winProbability = winProbability
+
+    results.push(championPerformance)
+  }
+
+  results.sort((a, b) => b.winProbability - a.winProbability)
   
-  return [];
+  return results;
 }
--- next/src/scripts/test-recommendation.ts
+++ next/src/scripts/test-recommendation.ts
@@ -1,11 +1,19 @@
 import db from '../lib/db'
+import { recommendChampions } from '../lib/champions/recommendationEngine'
 
 const main = async () => {
   try {
     const championStats = await db.query('SELECT * FROM champion_stats ORDER BY id DESC LIMIT 1')
-
     const lastChampionStats = championStats.rows[0].data
-    console.log(lastChampionStats['1'])
+
+    const champSelect = {
+      bans: [63],
+      allies: [51, 25],
+      enemies: [22, 117],
+    }
+
+    const result = await recommendChampions(lastChampionStats, champSelect)
+    console.log(result)
   } catch (error) { 
     console.error('Error connecting to db', error)
   } finally {
