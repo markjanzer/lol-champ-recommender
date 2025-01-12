6f37f9c972d3f44e15a8ceb6016d7fd9819a1885
Mark Janzer <mark.janzer@gmail.com>
2025-01-12 11:58:01 -0800

    Refactor recommendationEngine.ts

--- next/src/lib/champions/recommendationEngine.ts
+++ next/src/lib/champions/recommendationEngine.ts
@@ -1,4 +1,4 @@
-import { ChampionPerformance, ChampionInteraction, ChampionDataMap, ChampSelect } from '../types/champions';
+import { ChampionPerformance, WinStats, ChampionInteraction, ChampionDataMap, ChampSelect } from '../types/champions';
 
 export async function recommendChampions(
   championStats: ChampionDataMap,
@@ -6,88 +6,55 @@ export async function recommendChampions(
 ): Promise<ChampionPerformance[]> {
   const allChampIds = Object.keys(championStats).map(Number)
   const unavailableChampIds = [...champSelect.bans, ...champSelect.allies, ...champSelect.enemies]
+  const availableChampIds = allChampIds.filter(id => !unavailableChampIds.includes(id))
 
-  const results: ChampionPerformance[] = []
+  return availableChampIds
+    .map(champId => getChampionPerformance(champId, championStats, champSelect))
+    .sort((a, b) => b.winProbability - a.winProbability)
+}
 
-  for (const champId of allChampIds) {
-    if (unavailableChampIds.includes(champId)) {
-      continue
+function getChampionPerformance(champId: number, championStats: ChampionDataMap, champSelect: ChampSelect): ChampionPerformance {
+  const synergies = champSelect.allies.map(allyId => createInteraction(championStats[champId].synergies[allyId], allyId))
+  const matchups = champSelect.enemies.map(enemyId => createInteraction(championStats[champId].matchups[enemyId], enemyId)) 
+  const winProbability = calculateWinProbability([...synergies, ...matchups])
+
+  return {
+    championId: champId,
+    winProbability: winProbability,
+    synergies: synergies,
+    matchups: matchups,
+  }
+}
+
+function createInteraction(stats: WinStats, championId: number): ChampionInteraction {
+  if (stats === null) {
+    console.error(`Stats not found for champion ${championId}`)
+    return {
+      championId: championId,
+      winProbability: 0.50,
+      wins: 0,
+      games: 0,
     }
-
-    const championPerformance: ChampionPerformance = {
-      championId: champId,
-      winProbability: -1,
-      synergies: [],
-      matchups: [],
-    }
-
-    for (const allyId of champSelect.allies) {
-      const synergy = championStats[champId].synergies[allyId]
-      if (!synergy) {
-        console.error(`Synergy not found for champion ${champId} and ally ${allyId}`)
-        continue
-      }
-
-      let winProbability: number = 0.50
-      if (synergy.games > 0) {
-        // Smoothing winrate
-        winProbability = (synergy.wins + 5) / (synergy.games + 10)
-      }
-
-      const interaction: ChampionInteraction = {
-        championId: allyId,
-        winProbability: winProbability,
-        wins: synergy.wins,
-        games: synergy.games,
-      }
-
-      championPerformance.synergies.push(interaction)
-    }
-
-    for (const enemyId of champSelect.enemies) {
-      const matchup = championStats[champId].matchups[enemyId]
-      if (!matchup) {
-        console.error(`Matchup not found for champion ${champId} and enemy ${enemyId}`)
-        continue
-      }
-
-      let winProbability: number = 0.50
-      if (matchup.games > 0) {
-        winProbability = (matchup.wins + 5) / (matchup.games + 10)
-      }
-
-      const interaction: ChampionInteraction = {
-        championId: enemyId,
-        winProbability: winProbability,
-        wins: matchup.wins,
-        games: matchup.games,
-      }
-
-      championPerformance.matchups.push(interaction)
-    }
-
-    let winProbability: number = 0.0
-    const dataPoints = championPerformance.synergies.length + championPerformance.matchups.length
-    if (dataPoints > 0) {
-      for (const synergy of championPerformance.synergies) {
-        winProbability += synergy.winProbability
-      }
-
-      for (const matchup of championPerformance.matchups) {
-        winProbability += matchup.winProbability
-      }
-
-      winProbability /= dataPoints
-    } else {
-      winProbability = 0.50
-    }
-
-    championPerformance.winProbability = winProbability
-
-    results.push(championPerformance)
   }
 
-  results.sort((a, b) => b.winProbability - a.winProbability)
-  
-  return results;
+  let winProbability: number = 0.50
+  if (stats.games > 0) {
+    // Smoothing winrate
+    winProbability = (stats.wins + 5) / (stats.games + 10)
+  }
+
+  return {
+    championId: championId,
+    winProbability: winProbability,
+    wins: stats.wins,
+    games: stats.games,
+  }
+}
+
+function calculateWinProbability(interactions: ChampionInteraction[]): number {
+  if (interactions.length === 0) {
+    return 0.50
+  }
+
+  return interactions.reduce((sum, interaction) => sum + interaction.winProbability, 0) / interactions.length
 }
