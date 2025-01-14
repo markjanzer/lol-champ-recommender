375778111f85983898d098e43b098f5db6c7d4b2
Mark Janzer <mark.janzer@gmail.com>
2025-01-14 15:58:51 -0800

    Download libraries, move combobox component to its own file

--- next/src/components/ChampionCombobox.tsx
+++ next/src/components/ChampionCombobox.tsx
@@ -1,19 +1,26 @@
 'use client';
-import { useEffect, useState, Fragment } from "react";
 
 interface Props {
   champions: {
     name: string;
     api_id: number;
   }[];
+  onChange: (value: number) => void;
+  value: number;
 }
 
-export default function ChampionCombobox({champions}: Props) {
+export default function ChampionCombobox({champions, onChange, value}: Props) {
   return (
-    <select className="border border-gray-300 rounded-md p-2 text-black">
-      {champions.map(champion => (
-        <option key={champion.api_id} value={champion.api_id}>{champion.name}</option>
-      ))}
-    </select>
-  );
+    <div className="mt-2">
+      <input
+        className="border border-gray-300 rounded-md p-2 text-black"
+        type="number"
+        min={1}
+        max={10000}
+        value={value || ''}
+        onChange={(e) => onChange(parseInt(e.target.value) || 0)}
+      />
+      <span className="text-sm ml-2 font-bold">{champions.find(champion => champion.api_id === value)?.name}</span>
+    </div>
+  )
 }
--- next/src/components/ChampionRecommender.tsx
+++ next/src/components/ChampionRecommender.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState, Fragment } from "react";
 import { recommendChampions } from "@/lib/champions/recommendationEngine";
 import { ChampionDataMap, ChampionPerformance } from "@/lib/types/champions"
+import ChampionCombobox from "./ChampionCombobox";
 
 interface Props {
   championStats: ChampionDataMap;
@@ -9,31 +10,6 @@ interface Props {
     name: string;
     api_id: number;
   }[];
-}
-
-interface ChampionComboboxProps {
-  champions: {
-    name: string;
-    api_id: number;
-  }[];
-  onChange: (value: number) => void;
-  value: number;
-}
-
-function ChampionCombobox({champions, onChange, value}: ChampionComboboxProps) {
-  return (
-    <div className="mt-2">
-      <input
-        className="border border-gray-300 rounded-md p-2 text-black"
-        type="number"
-        min={1}
-        max={10000}
-          value={value || ''}
-        onChange={(e) => onChange(parseInt(e.target.value) || 0)}
-      />
-      <span className="text-sm ml-2 font-bold">{champions.find(champion => champion.api_id === value)?.name}</span>
-    </div>
-  )
 }
 
 export default function ChampionRecommender({championStats, champions}: Props) {
