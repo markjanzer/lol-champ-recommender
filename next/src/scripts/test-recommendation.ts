9c299dc1402d95c108e070921b7aa79ae31ef6dc
Mark Janzer <mark.janzer@gmail.com>
2025-01-21 11:24:54 -0800

    Fix js script again

--- next/src/scripts/test-recommendation.ts
+++ next/src/scripts/test-recommendation.ts
@@ -1,24 +1,17 @@
 import { recommendChampions } from '../lib/champions/recommendationEngine'
 import championStats from '@/data/champion_stats.json'
 
-const main = async () => {
-  try {
+const main = () => {
+  const lastChampionStats = championStats
 
-    const lastChampionStats = championStats
-
-    const champSelect = {
-      bans: [63],
-      allies: [3, 67],
-      enemies: [22, 117],
-    }
-
-    const result = recommendChampions(lastChampionStats, champSelect)
-    console.log(result)
-  } catch (error) { 
-    console.error('Error connecting to db', error)
-  } finally {
-    await db.end()
+  const champSelect = {
+    bans: [63],
+    allies: [3, 67],
+    enemies: [22, 117],
   }
+
+  const result = recommendChampions(lastChampionStats, champSelect)
+  console.log(result)
 }
 
 main()
